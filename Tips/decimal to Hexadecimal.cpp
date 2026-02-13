9c5cdacec61a07aa064e2f79d2ee3202f853b32b
Faysal Mahmud <95643831+Faysal3010@users.noreply.github.com>
2026-02-13 18:14:54 +0600

    u

---
+++ Tips/decimal to Hexadecimal.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a, b;
+    cin >> a;
+    while (a != 0)
+    {
+        b = a % 16;
+        a = a / 16;
+        if(b==10)cout<<'A';
+        else if(b==11)cout<<'B';
+        else if(b==12)cout<<'C';
+        else if(b==13)cout<<'D';
+        else if(b==14)cout<<'E';
+        else if(b==15)cout<<'F';
+        else cout<<b;
+    }
+}
