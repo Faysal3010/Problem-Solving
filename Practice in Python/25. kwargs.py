5334d92faf2f5636d3ea0e7e79f8cec974e43a0a
Faysal Mahmud <95643831+FaysalMahmudSajan@users.noreply.github.com>
2026-02-15 23:32:08 +0600

    upload

---
+++ Practice in Python/25. kwargs.py
@@ -0,0 +1,10 @@
+'''
+
+'''
+
+def hello(**kwargs):
+    print('Hello,',end=' ')
+    for key,value in kwargs.items():
+        print(value,end=' ')
+hello(title='Mr.',first='Faysal',last='Mahmud')        
+ 
